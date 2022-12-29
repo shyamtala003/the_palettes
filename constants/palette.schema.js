67dfce899194252f61000f1891a3292363851321
shyamtala003 <shyamtala003@gmail.com>
2022-12-29 11:26:08 +0530

    more color palettes added

--- constants/palette.schema.js
+++ constants/palette.schema.js
@@ -164,6 +164,144 @@ let colorPalettes = [{
 
         ]
     },
+
+    {
+        paletteImage: "./uiux_images/12.webp",
+        typefaceName: "gilroy",
+        paletteColors: ['#040a20', '#4285f4', '#186efc','#161d34','#ffffff'],
+        designer: 'taqwah_digital',
+        designerSocialLinks: [{
+                socialSiteIcon: 'instagram',
+                socialSiteUrl: "https://www.instagram.com/taqwah_digital/"
+            },
+
+        ]
+    },
+
+    {
+        paletteImage: "./uiux_images/13.webp",
+        typefaceName: "oxanium, work sans",
+        paletteColors: ['#1c121f', '#ffce4e', '#372839','#ffffff'],
+        designer: 'taqwah_digital',
+        designerSocialLinks: [{
+                socialSiteIcon: 'instagram',
+                socialSiteUrl: "https://www.instagram.com/taqwah_digital/"
+            },
+
+        ]
+    },
+
+    {
+        paletteImage: "./uiux_images/14.webp",
+        typefaceName: "gilroy",
+        paletteColors: ['#00e3a5', '#0d0d0d', '#80ab9e','#fafafa'],
+        designer: 'taqwah_digital',
+        designerSocialLinks: [{
+                socialSiteIcon: 'instagram',
+                socialSiteUrl: "https://www.instagram.com/taqwah_digital/"
+            },
+
+        ]
+    },
+
+    {
+        paletteImage: "./uiux_images/15.webp",
+        typefaceName: "exo, DM sans",
+        paletteColors: ['#170d18', '#f827de', '#2b14f9','#2faf32','#ffffff'],
+        designer: 'taqwah_digital',
+        designerSocialLinks: [{
+                socialSiteIcon: 'instagram',
+                socialSiteUrl: "https://www.instagram.com/taqwah_digital/"
+            },
+
+        ]
+    },
+
+    {
+        paletteImage: "./uiux_images/16.webp",
+        typefaceName: "syncopate, gilroy",
+        paletteColors: ['#040404', '#b9fd02', '#111518','#ccd2e3','#ffb801','#FF2F61'],
+        designer: 'taqwah_digital',
+        designerSocialLinks: [{
+                socialSiteIcon: 'instagram',
+                socialSiteUrl: "https://www.instagram.com/taqwah_digital/"
+            },
+
+        ]
+    },
+
+    {
+        paletteImage: "./uiux_images/17.webp",
+        typefaceName: "work sans",
+        paletteColors: ['#006dff', '#fdc526', '#ff76fa','#ffffff'],
+        designer: 'oyolloo',
+        designerSocialLinks: [{
+                socialSiteIcon: 'dribble',
+                socialSiteUrl: "https://dribbble.com/Oyolloo"
+            },
+
+        ]
+    },
+
+    {
+        paletteImage: "./uiux_images/18.webp",
+        typefaceName: "work sans",
+        paletteColors: ['#ffd70c','#0f1014','#ffffff','#4e4d45'],
+        designer: 'oyolloo',
+        designerSocialLinks: [{
+                socialSiteIcon: 'dribble',
+                socialSiteUrl: "https://dribbble.com/Oyolloo"
+            },
+
+        ]
+    },
+
+    {
+        paletteImage: "./uiux_images/19.webp",
+        typefaceName: "nunito sans",
+        paletteColors: ['#ffc335','#3affa5','#d1132d','#1c1c1c'],
+        designer: 'oyolloo',
+        designerSocialLinks: [{
+                socialSiteIcon: 'dribble',
+                socialSiteUrl: "https://dribbble.com/Oyolloo"
+            },
+
+        ]
+    },
+
+    {
+        paletteImage: "./uiux_images/20.webp",
+        typefaceName: "gotham",
+        paletteColors: ['#2ec8d9','#74e4f0','#dfe4f5','#f7f9ff'],
+        designer: 'Shafiqul Islam',
+        designerSocialLinks: [{
+                socialSiteIcon: 'instagram',
+                socialSiteUrl: "https://www.instagram.com/kitket_design/"
+            },
+            {
+                socialSiteIcon: 'dribble',
+                socialSiteUrl: "https://dribbble.com/kitket1212"
+            },
+
+        ]
+    },
+
+    {
+        paletteImage: "./uiux_images/21.webp",
+        typefaceName: "GT america",
+        paletteColors: ['#e09d4a','#443e39','#c2c2c2','#ffffff','#fbfbf2'],
+        designer: 'Shafiqul Islam',
+        designerSocialLinks: [{
+                socialSiteIcon: 'instagram',
+                socialSiteUrl: "https://www.instagram.com/kitket_design/"
+            },
+            {
+                socialSiteIcon: 'dribble',
+                socialSiteUrl: "https://dribbble.com/kitket1212"
+            },
+
+        ]
+    },
 ]
 
 
@@ -223,14 +361,14 @@ let addCard = (cardData) => {
             `
         <span class="text">Designed by <span class="author">${cardData.designer}</span></span>
         <div class="social_links">
-            <a href="${cardData.designerSocialLinks[0].socialSiteUrl}" aria-label="${cardData.designer}:instagram"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
+            <a href="${cardData.designerSocialLinks[0].socialSiteUrl}" aria-label="${cardData.designer}:instagram" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
                     height="24">
                     <path fill="none" d="M0 0h24v24H0z" />
                     <path class="svg_icon"
                         d="M12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm0-2a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm6.5-.25a1.25 1.25 0 0 1-2.5 0 1.25 1.25 0 0 1 2.5 0zM12 4c-2.474 0-2.878.007-4.029.058-.784.037-1.31.142-1.798.332-.434.168-.747.369-1.08.703a2.89 2.89 0 0 0-.704 1.08c-.19.49-.295 1.015-.331 1.798C4.006 9.075 4 9.461 4 12c0 2.474.007 2.878.058 4.029.037.783.142 1.31.331 1.797.17.435.37.748.702 1.08.337.336.65.537 1.08.703.494.191 1.02.297 1.8.333C9.075 19.994 9.461 20 12 20c2.474 0 2.878-.007 4.029-.058.782-.037 1.309-.142 1.797-.331.433-.169.748-.37 1.08-.702.337-.337.538-.65.704-1.08.19-.493.296-1.02.332-1.8.052-1.104.058-1.49.058-4.029 0-2.474-.007-2.878-.058-4.029-.037-.782-.142-1.31-.332-1.798a2.911 2.911 0 0 0-.703-1.08 2.884 2.884 0 0 0-1.08-.704c-.49-.19-1.016-.295-1.798-.331C14.925 4.006 14.539 4 12 4zm0-2c2.717 0 3.056.01 4.122.06 1.065.05 1.79.217 2.428.465.66.254 1.216.598 1.772 1.153a4.908 4.908 0 0 1 1.153 1.772c.247.637.415 1.363.465 2.428.047 1.066.06 1.405.06 4.122 0 2.717-.01 3.056-.06 4.122-.05 1.065-.218 1.79-.465 2.428a4.883 4.883 0 0 1-1.153 1.772 4.915 4.915 0 0 1-1.772 1.153c-.637.247-1.363.415-2.428.465-1.066.047-1.405.06-4.122.06-2.717 0-3.056-.01-4.122-.06-1.065-.05-1.79-.218-2.428-.465a4.89 4.89 0 0 1-1.772-1.153 4.904 4.904 0 0 1-1.153-1.772c-.248-.637-.415-1.363-.465-2.428C2.013 15.056 2 14.717 2 12c0-2.717.01-3.056.06-4.122.05-1.066.217-1.79.465-2.428a4.88 4.88 0 0 1 1.153-1.772A4.897 4.897 0 0 1 5.45 2.525c.638-.248 1.362-.415 2.428-.465C8.944 2.013 9.283 2 12 2z"
                         fill="rgba(255,255,255,1)" /></svg>
             </a>
-            <a href="${cardData.designerSocialLinks[1].socialSiteUrl}" aria-label="${cardData.designer}:dribble"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
+            <a href="${cardData.designerSocialLinks[1].socialSiteUrl}" aria-label="${cardData.designer}:dribble" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
                     height="24">
                     <path fill="none" d="M0 0h24v24H0z" />
                     <path class="svg_icon"
@@ -245,7 +383,7 @@ let addCard = (cardData) => {
                 `
         <span class="text">Designed by <span class="author">${cardData.designer}</span></span>
         <div class="social_links">
-            <a href="${cardData.designerSocialLinks[0].socialSiteUrl}" aria-label="${cardData.designer}:instagram">
+            <a href="${cardData.designerSocialLinks[0].socialSiteUrl}" aria-label="${cardData.designer}:instagram" target="_blank">
             <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
             height="24">
             <path fill="none" d="M0 0h24v24H0z" />
@@ -260,7 +398,7 @@ let addCard = (cardData) => {
                 `
             <span class="text">Designed by <span class="author">${cardData.designer}</span></span>
             <div class="social_links">
-                <a href="${cardData.designerSocialLinks[0].socialSiteUrl}" aria-label="${cardData.designer}:dribble">
+                <a href="${cardData.designerSocialLinks[0].socialSiteUrl}" aria-label="${cardData.designer}:dribble" target="_blank">
                 <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
                     height="24">
                     <path fill="none" d="M0 0h24v24H0z" />
