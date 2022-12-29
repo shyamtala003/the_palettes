a06744d106c4c35a78c9c7742d7cb1fa3396153a
shyamtala003 <shyamtala003@gmail.com>
2022-12-29 14:04:03 +0530

    light mode added with some css fixes

--- main.js
+++ main.js
@@ -1,21 +1,64 @@
+// code for dark/white mode toggler
+
+let themeToggler = document.querySelectorAll('.theme_toggler');
+themeToggler.forEach(element => {
+  element.addEventListener('click', () => {
+    toggleTheme(element);
+  })
+});
+
+let toggleTheme = (element) => {
+
+  let menuTogglerBtn = document.getElementById('menu_toggler');
+
+  if (element.dataset.theme == "dark") {
+    element.dataset.theme = "light";
+    document.body.classList.add("light_theme");
+    element.firstElementChild.setAttribute("src", "./icons/moon.webp");
+
+    menuTogglerBtn.firstChild.setAttribute('src', "./icons/close_dark.webp");
+  } else {
+    element.dataset.theme = "dark";
+    document.body.classList.remove("light_theme");
+    element.firstElementChild.setAttribute("src", "./icons/sun.webp");
+
+    menuTogglerBtn.firstChild.setAttribute('src', "./icons/close-light.webp");
+  }
+}
+
+
+// code for responsive menu open close 
+
 let menuBtn = document.getElementById('menu_toggler');
 
-menuBtn.onclick=()=>{
-    let btnIcon = menuBtn.firstElementChild;
-    let resonsiveMenu=document.querySelector(".responsive_menu");
-    if(btnIcon.getAttribute("src")==="./icons/menu_light.webp")
-    {
-        setTimeout(() => {
-            btnIcon.src=`./icons/close-light.webp`;
-        }, 400);
-        resonsiveMenu.classList.add("open_navbar");
-    }else
-    {
-        setTimeout(() => {
-            btnIcon.setAttribute("src","./icons/menu_light.webp");
-        }, 400);
-        resonsiveMenu.classList.remove("open_navbar");
+menuBtn.onclick = () => {
+  let btnIcon = menuBtn.firstElementChild;
+  let resonsiveMenu = document.querySelector(".responsive_menu");
+  if (document.body.classList.contains("light_theme")) {
+    if (btnIcon.getAttribute("src") === "./icons/menu_light.webp" || btnIcon.getAttribute("src") === "./icons/menu_dark.webp") {
+      setTimeout(() => {
+        btnIcon.src = `./icons/close_dark.webp`;
+      }, 400);
+      resonsiveMenu.classList.add("open_navbar");
+    } else {
+      setTimeout(() => {
+        btnIcon.setAttribute("src", "./icons/menu_dark.webp");
+      }, 400);
+      resonsiveMenu.classList.remove("open_navbar");
     }
+  } else {
+    if (btnIcon.getAttribute("src") === "./icons/menu_light.webp" || btnIcon.getAttribute("src") === "./icons/menu_dark.webp") {
+      setTimeout(() => {
+        btnIcon.src = `./icons/close-light.webp`;
+      }, 400);
+      resonsiveMenu.classList.add("open_navbar");
+    } else {
+      setTimeout(() => {
+        btnIcon.setAttribute("src", "./icons/menu_light.webp");
+      }, 400);
+      resonsiveMenu.classList.remove("open_navbar");
+    }
+  }
 }
 
 
